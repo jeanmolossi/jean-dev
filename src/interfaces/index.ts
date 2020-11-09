d9d02df8888f3681a8db51893f54a020b60d4978
jeanmolossi <jean.carlosmolossi@gmail.com>
2020-11-09 01:01:46 -0300

    New: Types added

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -5,6 +5,59 @@
 // import User from 'path/to/interfaces';
 
 export type User = {
-  id: number
-  name: string
+  username: string;
+  email: string;
+  confirmed: boolean;
+  blocked: boolean;
+  role: number;
+} & Omit<ContentEssentials, 'published_at'>
+
+export type ContentEssentials = {
+  id: number;
+  created_at: string;
+  updated_at: string;
+  published_at: string;
 }
+
+export type ImageFormat = {
+  ext: string;
+  hash: string;
+  height: number;
+  width: number;
+  mime: string;
+  name: string;
+  size: number;
+  url: string;
+}
+
+export type ImageData = {
+  name: string;
+  mime: string;
+  url: string;
+  ext: string;
+  caption: string;
+  formats: {
+    large: ImageFormat;
+    medium: ImageFormat;
+    small: ImageFormat;
+    thumbnail: ImageFormat;
+  };
+  hash: string;
+  height: number;
+  provider: string;
+  size: number;
+} & Omit<ContentEssentials, 'published_at'>;
+
+export type Category = {
+  name: string;
+  slug: string;  
+} & ContentEssentials;
+
+export type Post = {
+  title: string;
+  post: string;
+  slug: string;
+  image: ImageData[];
+  category: Category;
+  user: User;
+} & ContentEssentials;
