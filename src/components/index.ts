8dff1f4265fa86309dc8f5c0e3520a5385103829
jeanmolossi <jean.carlosmolossi@gmail.com>
2020-11-09 01:01:15 -0300

    New: Pages added

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,5 @@ export { default as SpanMarker } from './shared/SpanMarker';
 
 export { default as Layout } from './Layout';
 export { default as Home } from './pages/Home';
+export { default as Blog } from './pages/Blog';
+export { default as Post } from './pages/Post';
